db71c133e8f28482042aab4471de45b28f050054
Kevin Moore <kevmoo@google.com>
2016-08-17 09:20:08 -0700

    Enable travis CI
    Requires environment variables set on Travis
    Closes https://github.com/Janamou/firebase3-dart/issues/5
    
    Also require Dart 1.18 – there have been changes to JS

---
+++ tool/create_config.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+final _assetPath = 'lib/src/assets/';
+
+main() {
+  // make sure the working dir is the root of the project
+  if (!(new File('pubspec.yaml').existsSync())) {
+    throw "Not in the root! - ${Directory.current}";
+  }
+
+  var samplePath = p.join(_assetPath, 'config.json.sample');
+  if (!(new File(samplePath).existsSync())) {
+    throw "'${samplePath} should exist";
+  }
+
+  var configPath = p.join(_assetPath, 'config.json');
+  var configFile = new File(configPath);
+
+  if (configFile.existsSync()) {
+    throw "Config exists. It should not. '$configPath'";
+  }
+
+  var vars = ["API_KEY", "AUTH_DOMAIN", "DATABASE_URL", "STORAGE_BUCKET"];
+
+  var config = <String, String>{};
+  for (var envVar in vars) {
+    if (Platform.environment.containsKey(envVar)) {
+      config[envVar] = Platform.environment[envVar];
+    } else {
+      throw 'Missing needed environment variable $envVar';
+    }
+  }
+
+  configFile
+      .writeAsStringSync(new JsonEncoder.withIndent('  ').convert(config));
+}
