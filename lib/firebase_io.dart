eff1c88bbe5b66b9522b7794278233c8878fa40f
Jana Moudrá <jana.moudra@gmail.com>
2016-12-04 18:03:50 +0100

    IO documentation

--- lib/firebase_io.dart
+++ lib/firebase_io.dart
@@ -3,26 +3,55 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 
+/// FirebaseClient wraps a REST client for a Firebase realtime database.
+///
+/// The client supports authentication and GET, PUT, POST, DELETE
+/// and PATCH methods.
 class FirebaseClient {
+  /// Auth credential.
   final String credential;
   final BaseClient _client;
 
+  /// Creates a new FirebaseClient with [credential] and optional [client].
+  ///
+  /// For credential you can either use Firebase app's secret or
+  /// an authentication token.
+  /// See: <https://firebase.google.com/docs/reference/rest/database/user-auth>.
   FirebaseClient(this.credential, {BaseClient client})
       : _client = client ?? new IOClient();
 
+  /// Creates a new anonymous FirebaseClient with optional [client].
   FirebaseClient.anonymous({BaseClient client})
       : credential = null,
         _client = client ?? new IOClient();
 
+  /// Reads data from database using a HTTP GET request.
+  /// The response from a successful request contains a data being retrieved.
+  ///
+  /// See: <https://firebase.google.com/docs/reference/rest/database/#section-get>.
   Future<dynamic> get(uri) => send('GET', uri);
 
+  /// Writes or replaces data in database using a HTTP PUT request.
+  /// The response from a successful request contains a data being written.
+  ///
+  /// See: <https://firebase.google.com/docs/reference/rest/database/#section-put>.
   Future<dynamic> put(uri, json) => send('PUT', uri, json: json);
 
+  /// Pushes data to database using a HTTP POST request.
+  /// The response from a successful request contains a key of the new data
+  /// being added.
+  ///
+  /// See: <https://firebase.google.com/docs/reference/rest/database/#section-post>.
   Future<dynamic> post(uri, json) => send('POST', uri, json: json);
 
+  /// Deletes data from database using a HTTP DELETE request.
+  /// The response from a successful request contains a JSON with [:null:].
+  ///
+  /// See: <https://firebase.google.com/docs/reference/rest/database/#section-delete>.
   Future<dynamic> delete(uri) => send('DELETE', uri);
 
-  /// [url] can be either a `String` or `Uri`.
+  /// Creates a request with a HTTP [method], [url] and optional data.
+  /// The [url] can be either a `String` or `Uri`.
   Future<dynamic> send(String method, url, {json}) async {
     Uri uri = url is String ? Uri.parse(url) : url;
 
@@ -65,5 +94,6 @@ class FirebaseClient {
     return bodyJson;
   }
 
+  /// Closes the client and cleans up any associated resources.
   void close() => _client.close();
 }
