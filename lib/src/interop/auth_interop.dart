17c0f3fda237a836c7188e866f8304ef2539c241
Jana Moudrá <jana.moudra@gmail.com>
2017-10-28 23:21:45 +0200

    fmt

--- lib/src/interop/auth_interop.dart
+++ lib/src/interop/auth_interop.dart
@@ -170,6 +170,7 @@ abstract class UserMetadata {
   /// The date the user was created, formatted as a UTC string.
   /// For example, 'Fri, 22 Sep 2017 01:49:58 GMT'.
   external String get creationTime;
+
   /// The date the user last signed in, formatted as a UTC string.
   /// For example, 'Fri, 22 Sep 2017 01:49:58 GMT'.
   external String get lastSignInTime;
