f1e9c81d0c40233fdabf049ca6f6145179fcfdb8
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2018-10-03 20:29:59 +0200

    Spacing

--- lib/src/interop/database_interop.dart
+++ lib/src/interop/database_interop.dart
@@ -62,7 +62,8 @@ abstract class QueryJsImpl {
   external bool isEqual(QueryJsImpl other);
   external QueryJsImpl limitToFirst(int limit);
   external QueryJsImpl limitToLast(int limit);
-  external void off([String eventType, Func2Opt1<DataSnapshotJsImpl, String, Null>  callback, context]);
+  external void off([String eventType,
+    Func2Opt1<DataSnapshotJsImpl, String, Null>  callback, context]);
   external Func0 on(
       String eventType, Func2Opt1<DataSnapshotJsImpl, String, Null> callback,
       [cancelCallbackOrContext, context]);
