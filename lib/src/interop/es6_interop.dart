e19064f75e3d5e7e1de84f02ef413f8f09b01808
Kevin Moore <kevmoo@google.com>
2019-07-22 11:36:46 -0700

    fix formatting

--- lib/src/interop/es6_interop.dart
+++ lib/src/interop/es6_interop.dart
@@ -6,8 +6,7 @@ import 'package:js/js.dart';
 import '../func.dart';
 
 @JS('Promise')
-class PromiseJsImpl<T>  {
+class PromiseJsImpl<T> {
   external PromiseJsImpl(Function resolver);
   external PromiseJsImpl then([Func1 onResolve, Func1 onReject]);
 }
-
