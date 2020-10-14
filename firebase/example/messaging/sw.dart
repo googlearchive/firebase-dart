7ef81823e9f0ab0995255d389e0af017e4771bd1
Kevin Moore <kevmoo@users.noreply.github.com>
2020-10-14 12:40:11 -0700

    Organize repo into a mono repo (#348)
    Eliminates files from `lib` that should never have been there

---
+++ firebase/example/messaging/sw.dart
@@ -0,0 +1,29 @@
+@JS()
+library messaging_demo.service_worker;
+
+import 'package:firebase/firebase.dart' as firebase;
+import 'package:js/js.dart';
+import 'package:service_worker/worker.dart' as sw;
+import 'package:shared_assets/assets.dart';
+
+void main(List<String> args) async {
+  importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+  importScripts(
+      'https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js');
+
+  await config();
+
+  firebase.initializeApp(
+    projectId: projectId,
+    messagingSenderId: messagingSenderId,
+  );
+
+  final messaging = firebase.messaging();
+  messaging.onBackgroundMessage.listen((payload) {
+    final options = sw.ShowNotificationOptions(body: payload.notification.body);
+    sw.registration.showNotification(payload.notification.title, options);
+  });
+}
+
+@JS('importScripts')
+external void importScripts(String url);
---
+++ firebase/test/io_test.dart
@@ -0,0 +1,95 @@
+@TestOn('vm')
+import 'package:firebase/firebase_io.dart';
+import 'package:http/http.dart';
+import 'package:shared_assets/assets_io.dart';
+import 'package:test/test.dart';
+
+import 'test_util.dart';
+
+void main() {
+  String databaseUri;
+  FirebaseClient fbClient;
+  String testUri;
+
+  setUpAll(() async {
+    var token = await getAccessToken(Client());
+    fbClient = FirebaseClient(token.data);
+    databaseUri = await getDatabaseUri();
+  });
+
+  setUp(() {
+    var path = validDatePath();
+    testUri = '$databaseUri/$path.json';
+  });
+
+  tearDown(() async {
+    await fbClient.delete(testUri);
+    testUri = null;
+  });
+
+  tearDownAll(() {
+    if (fbClient != null) {
+      fbClient.close();
+    }
+  });
+
+  test('never-accessed path is null', () async {
+    var response = await fbClient.get(testUri);
+    expect(response, isNull);
+  });
+
+  test('Uri is fine, too', () async {
+    var response = await fbClient.get(Uri.parse(testUri));
+    expect(response, isNull);
+  });
+
+  test('put', () async {
+    var response = await fbClient.put(testUri, 'bob');
+    expect(response, 'bob');
+
+    response = await fbClient.get(testUri);
+    expect(response, 'bob');
+  });
+
+  test('post', () async {
+    var response = await fbClient.post(testUri, 'bob') as Map;
+    expect(response, contains('name'));
+
+    var key = response['name'];
+
+    response = await fbClient.get(testUri);
+    expect(response, {key: 'bob'});
+  });
+
+  test('patch', () async {
+    var response = await fbClient.patch(testUri, {'someNewKey': 'bob'}) as Map;
+    expect(response, contains('someNewKey'));
+
+    var key = 'someNewKey';
+
+    response = await fbClient.get(testUri);
+    expect(response, {key: 'bob'});
+  });
+
+  test('cleanup', () async {
+    final oneMinuteOld = DateTime.now().subtract(const Duration(minutes: 1));
+
+    final rootPath = '$databaseUri/pkg_firebase_test.json';
+    final response = await fbClient.get(rootPath) as Map<String, dynamic>;
+
+    if (response == null) {
+      return;
+    }
+    for (var entry in response.entries) {
+      if (entry.key.contains('T')) {
+        final oldUri = '$databaseUri/pkg_firebase_test/${entry.key}.json';
+        print(oldUri);
+        final split = entry.key.split('T');
+        final date = DateTime.tryParse(split.first);
+        if (date != null && date.isBefore(oneMinuteOld)) {
+          await fbClient.delete(oldUri);
+        }
+      }
+    }
+  });
+}
